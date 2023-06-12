4143d089df4e2634d23d7adb5cf9e846d7607ea2
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-12 04:38:50 +0300

    Update

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,3 +1,10 @@
 function redirectToPage(url) {
-    window.location.href = url;
+    window.location.href = url
+}
+
+function formSubmit() {
+    var password = document.getElementById('password')
+    const hashed = crypto.createHash('sha-256').update(x).digest('hex')
+
+    password.value = hashed
 }
